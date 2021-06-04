1dfadaed0224079982239d3fa500bc3b0c0179a0
haziqhafizuddin <haziqhafizuddintamsir@gmail.com>
2021-06-04 21:48:51 +0800

    setting up tailwind css

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/**/*.html.haml",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.vue",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
